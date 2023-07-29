d4cbc2ecf870a13c309ddb5cd7790b5a6866c505
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-07-29 18:31:22 +0300

    Image property

--- app/src/main/java/com/omrilhn/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/omrilhn/statemanagementcompose/MainActivity.kt
@@ -3,9 +3,16 @@ package com.omrilhn.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,7 +20,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.omrilhn.statemanagementcompose.ui.theme.StateManagementComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +47,38 @@ fun MainScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Text(text = "ANDROID")
+            Text(text = "ANDROID",
+                fontSize = 26.sp
+            )
+
+            Spacer(modifier = Modifier.padding(15.dp))
+
+            Button(onClick = {
+                println("Button clicked")
+            }) {
+                Text(text = "Test")
+                Text(text = "Button")
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.duman),
+                contentDescription ="Duman",
+                modifier = Modifier
+                    .width(320.dp)
+                    .height(240.dp))
+
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            Image(imageVector = ImageVector.vectorResource
+                (id = R.drawable.ic_launcher_foreground),
+                contentDescription = null)
+
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            Image(painter = ColorPainter(Color.LightGray),
+                contentDescription =null,
+                modifier = Modifier.size(10.dp,10.dp)//Set size of this PAINTED BOX
+                )
         }
     }
 
